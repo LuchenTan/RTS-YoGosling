63aa9f1c1da6baafb9163ca4d42e6e6e66741df9
Charles Qi <ccqi@uwaterloo.ca>
2018-02-12 00:00:21 -0500

    Added kafka consumer

---
+++ config/consumer_config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+# kafka topic to read from
+topic = 'tweets_api'
+
---
+++ twitter_kafka_consumer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+import json
+import config.consumer_config as config
+
+topic = config.topic
+
+consumer = KafkaConsumer(value_deserializer=lambda v: json.loads(v.decode('utf-8')))
+consumer.subscribe([topic])
+
+for message in consumer:
+    tweet = message.value
+    print (tweet)
+    # process each tweet
