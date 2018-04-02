577ebf77ffa5d58f17f543fa983b5af0aa5be18b
Charles Qi <ccqi@uwaterloo.ca>
2018-04-02 01:54:45 -0400

    wSeparated page crawler from integration test

--- Runs/page_crawler_test.py
+++ Runs/page_crawler_test.py
@@ -11,18 +11,8 @@ import sys
 
 runNo = "run0"
 
-
 if len(sys.argv) > 1:
     runNo = sys.argv[1]
-
-# Load Profiles
-rts16_path = "profiles/TREC2016-RTS-topics.json"
-rts17_path = "profiles/RTS17-topics.json"
-profile = TRECProfile.TRECProfileReader(rts17_path, 17)
-profile.read()
-queryset = dict()
-for topid, topic in profile.topics.items():
-    queryset[topid] = QueryGeneration.QueryGeneration(topic).getQuery()
 
 # Load tweets
 topic = config.topic
@@ -35,30 +25,24 @@ consumer = KafkaConsumer(value_deserializer=lambda v: json.loads(v.decode('utf-8
 partition = TopicPartition(topic, 0)
 consumer.assign([partition])
 
+
 offset = 0
 
 with open("last_offset.txt") as f:
     offset = int(f.read())
 
 consumer.seek(partition, offset)
-dictlimit = dict.fromkeys(queryset.keys(), 10)
-day = -1
+
+producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+                         batch_size=50000)
+topic = 'tweet_url_titles'
 
 for message in consumer:
     tweet = message.value
     # process each tweet
     tweetjson = prePro.process(tweet)
     if tweetjson:
-        time = int(round(float(tweetjson['timestamp_ms'])/1000))
-        newDay = datetime.datetime.fromtimestamp(time).day
-        if newDay != day:
-            day = newDay
-            for topid in dictlimit:
-                dictlimit[topid] = 10             
-        for topid, query in queryset.items():
-            query = queryset[topid]
-            if tm.match(query, tweetjson):
-                if dictlimit[topid] > 0:
-                    print(topid, tweetjson['id'], time, runNo)
-                    sys.stdout.flush()
-                    dictlimit[topid]-=1
+        urls = tweetjson['urls']
+        titles = pageCrawler.pageCrawler(urls)
+        if len(titles) > 0:
+            producer.send(topic, {'id': tweetjson['id'], 'titles':titles}) 
