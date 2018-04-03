874148c797740ff501474d1a0e70f63acb6d47ae
Charles Qi <ccqi@uwaterloo.ca>
2018-04-03 12:25:36 -0400

    partly working joining titles

--- Relevance/simpleTitleMatch.py
+++ Relevance/simpleTitleMatch.py
@@ -4,4 +4,12 @@ def match(query, tweetJson):
     tweets = tweetJson['normal_words']
     titles = query['title']
     return all(title in tweets for title in titles.keys())
-    
+
+
+def match_titles(query, titles):
+    query_titles = query['title']
+    tokens = []
+    for title in titles:
+        words = title.split(" ")
+        tokens.update(words)
+    return all(title in tokens for title in query_titles.keys())
