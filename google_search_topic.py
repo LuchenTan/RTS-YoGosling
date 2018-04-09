2e2ed32cf2f6c2c5b3fd200a5d33ad3dbc737ae5
Yinan Zhang <yinancy02@gmail.com>
2018-04-09 19:34:49 -0400

    Reading topics from RTS17-topics.json and write search results to topic_descriptions.json

--- google_search_topic.py
+++ google_search_topic.py
@@ -1,12 +1,36 @@
+import json
 from google import google
+
 num_page = 1
 
-class GoogleSearch:
-	def google_search_topic(topic):
-		search_results = google.search(topic, num_page)
+topic16_file = 'profiles/TREC2016-RTS-topics.json'
+topic17_file = 'profiles/RTS17-topics.json'
+
+
+with open('profiles/RTS17-topics.json', encoding='utf-8') as topic_file:
+	#print(topic_file.read())
+	for line in topic_file:
+		topic = json.loads(line)
+		#print(topic)
+		topic_title = topic.get('title', '')
+		topic_id = topic.get('topid', '')
+		topic_search_result = {}
+		descriptions = []
+		search_results = google.search(topic_title, num_page)
 		for result in search_results:
-			print(result.description)
-			print("\n")
+			try:
+				if (result.description):
+				    descriptions.append(result.description)
+			except Exception as e:
+				pass
+		topic_search_result['topid'] = topic_id
+		topic_search_result['descriptions'] = descriptions
+
+		with open('topic_descriptions.json', 'a') as outfile:
+			json.dump(topic_search_result, outfile)
+			outfile.write("\n")
+
+	
 
 
 
